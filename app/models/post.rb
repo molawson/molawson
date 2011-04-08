6bd9e0ed346cc16d381eb8039417e670ec5a774c
Mo Lawson <moklawson@gmail.com>
2011-04-08 14:14:33 -0500

    better slug creation/updating. a little view tweaking. no more js needed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ActiveRecord::Base
 
   validates_presence_of :title, :content
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :slug
+  
+  before_save :update_slug
 
   def self.published
     where("is_active = ?", true)
+  end
+  
+  private #-----------------------
+  
+  def update_slug
+    self.slug = self.title.downcase.gsub(/\s/ , '-').gsub(/[^a-zA-Z0-9-]+/,'')
   end
 end
