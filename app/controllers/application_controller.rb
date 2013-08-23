820c6c2b92fb0b8cc578a700f6fd2652f840e40b
Mo Lawson <moklawson@gmail.com>
2013-08-23 10:54:46 +0100

    Update design and copy slightly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   private #--------------------
-  
+
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == ADMIN_LOGIN['USERNAME'] && password == ADMIN_LOGIN['PASSWORD']
