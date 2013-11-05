07be6ef93aa53fc31e65ccfca66d184b63e25ea7
Mo Lawson <moklawson@gmail.com>
2013-11-05 10:52:53 +0000

    Add configuration for Heroku deploys

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,16 @@
-root = "/srv/apps/molawson/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
-listen "/tmp/unicorn.molawson.sock"
-worker_processes 2
-timeout 30
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+end
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/srv/apps/molawson/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -l /tmp/unicorn.molawson.sock -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deploy
 set -u
 
